0c5411753287719fcad7e09cf6d2a8eab4a167cb
Jeppe Stærk <jeppe@staerk.io>
2018-01-18 20:58:20 +0100

    Equal Sides Of An Array

---
+++ src/main/java/kata/kyu6/EqualSidesOfAnArray.java
@@ -0,0 +1,14 @@
+package kata.kyu6;
+
+public class EqualSidesOfAnArray {
+    public static int findEvenIndex(int[] arr) {
+        int index = 0, sumLeft = 0, sumRight = 0;
+        while (index++ < arr.length - 1) {
+            sumLeft = sumRight = 0;
+            for (int i = 0; i < index; i++) sumLeft += arr[i];
+            for (int j = arr.length - 1; j > index; j--) sumRight += arr[j];
+            if (sumLeft == sumRight) return index;
+        }
+        return -1;
+    }
+}
