df07077f53988dd2493a0f68e151d81bf91a356f
Jeppe Stærk <jeppe@staerk.io>
2018-01-18 21:10:03 +0100

    Multiply

---
+++ src/main/java/kata/kyu8/Multiply.java
@@ -0,0 +1,7 @@
+package kata.kyu8;
+
+public class Multiply {
+    public static Double multiply(Double a, Double b) {
+        return a * b;
+    }
+}
---
+++ src/test/java/kata/kyu8/MultiplyTest.java
@@ -0,0 +1,10 @@
+package kata.kyu8;
+
+import org.junit.Test;
+
+public class MultiplyTest {
+    @Test
+    public void testSomething() {
+        // assertEquals("expected", "actual");
+    }
+}
