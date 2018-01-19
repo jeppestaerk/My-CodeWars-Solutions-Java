b338144f10c4480ef5d763d196afb01a9cfb1a4d
Jeppe Stærk <jeppe@staerk.io>
2018-01-19 17:17:10 +0100

    Vasya - Clerk

---
+++ src/main/java/kata/kyu6/VasyaClerk.java
@@ -0,0 +1,28 @@
+package kata.kyu6;
+
+public class VasyaClerk {
+    public static String Tickets(int[] peopleInLine) {
+        int d25 = 0;
+        int d50 = 0;
+        int d100 = 0;
+        for (int i = 0; i < peopleInLine.length; i++) {
+            if (peopleInLine[i] == 25) d25++;
+            if (peopleInLine[i] == 50) {
+                d25--;
+                d50++;
+            }
+            if (peopleInLine[i] == 100) {
+                if (d50 > 0) {
+                    d50--;
+                    d25--;
+                    d100++;
+                } else {
+                    d25 -= 3;
+                    d100++;
+                }
+            }
+            if (d25 < 0 || d50 < 0) return "NO";
+        }
+        return "YES";
+    }
+}
---
+++ src/test/java/kata/kyu6/VasyaClerkTest.java
@@ -0,0 +1,17 @@
+package kata.kyu6;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class VasyaClerkTest {
+    @Test
+    public void test1() {
+        assertEquals("YES", VasyaClerk.Tickets(new int[]{25, 25, 50}));
+    }
+
+    @Test
+    public void test2() {
+        assertEquals("NO", VasyaClerk.Tickets(new int[]{25, 100}));
+    }
+}
