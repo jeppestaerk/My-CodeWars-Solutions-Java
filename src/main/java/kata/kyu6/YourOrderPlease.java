779a915601196f4303e3a89d01dbfe8d6622788b
Jeppe Stærk <jeppe@staerk.io>
2018-01-22 23:07:37 +0100

    Refactor

--- src/main/java/kata/kyu6/YourOrderPlease.java
+++ src/main/java/kata/kyu6/YourOrderPlease.java
@@ -3,12 +3,12 @@ package kata.kyu6;
 public class YourOrderPlease {
     public static String order(String words) {
         String[] strarr = words.split(" ");
-        String result = "";
+        StringBuilder sb = new StringBuilder();
         for (int i = 1; i <= strarr.length; i++) {
             for (String str : strarr) {
-                if (str.contains(Integer.toString(i))) result = result + str + " ";
+                if (str.contains(Integer.toString(i))) sb.append(str).append(" ");
             }
         }
-        return result.trim();
+        return sb.toString().trim();
     }
 }
