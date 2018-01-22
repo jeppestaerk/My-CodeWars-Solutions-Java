69bbae3b1de890e99d1e2b9bc03bd80acbcdbd30
Jeppe Stærk <jeppe@staerk.io>
2018-01-22 23:46:18 +0100

    Refactor

--- src/main/java/kata/kyu4/SquareIntoSquaresProtectTrees.java
+++ src/main/java/kata/kyu4/SquareIntoSquaresProtectTrees.java
@@ -6,48 +6,40 @@ import java.util.stream.Collectors;
 
 public class SquareIntoSquaresProtectTrees {
 
-    @SuppressWarnings("Duplicates")
     public String decompose(long n) {
 
-        long nSquare = (long) Math.pow(n, 2);
-        long sum = 0;
+        int index;
         List<Long> numbers = new ArrayList<>();
+        long nSquare = (long) Math.pow(n, 2);
+        long sum = fillList(n, nSquare, 0, numbers);
+        index = numbers.size();
+        while (sum != nSquare && index > 0) {
+            n = numbers.get(--index);
+            numbers = numbers.subList(0, index);
+            sum = getSum(numbers);
+            sum = fillList(n, nSquare, sum, numbers);
+            if (index == 0) index = numbers.size();
+        }
+        return (getSum(numbers) == nSquare) ? numbers.stream().sorted().map(Object::toString).collect(Collectors.joining(" ")) : null;
+    }
 
+    private long fillList(long n, long nSquare, long sum, List<Long> numbers) {
         while (sum < nSquare && --n > 0) {
-            long restSum = (nSquare - sum);
-            long nextNum = nextNumber(restSum, n);
+            long nextNum = nextNumber((nSquare - sum), n);
             if (!numbers.contains(nextNum)) {
                 numbers.add(nextNum);
                 n = nextNum;
                 sum += (long) Math.pow(nextNum, 2);
             }
         }
-
-        int index = numbers.size();
-        while (sum != nSquare && index > 0) {
-            sum = 0;
-            n = numbers.get(--index);
-            numbers = numbers.subList(0, index);
-            for (long num : numbers) sum += (long) Math.pow(num, 2);
-
-            while (sum < nSquare && --n > 0) {
-                long restSum = (nSquare - sum);
-                long nextNum = nextNumber(restSum, n);
-                if (!numbers.contains(nextNum)) {
-                    numbers.add(nextNum);
-                    n = nextNum;
-                    sum += (long) Math.pow(nextNum, 2);
-                }
-            }
-            if (index == 0) index = numbers.size();
-        }
-
-        sum = 0;
-        for (long num : numbers) sum += (long) Math.pow(num, 2);
-
-        return (sum == nSquare) ? numbers.stream().sorted().map(Object::toString).collect(Collectors.joining(" ")) : null;
+        return sum;
     }
 
+    private long getSum(List<Long> numbers) {
+        long sum = 0;
+        for (long num : numbers) sum += (long) Math.pow(num, 2);
+        return sum;
+    }
     private long nextNumber(long restSum, long n) {
         long tempSum = (long) Math.pow(n, 2);
         while (n > 0 && tempSum > restSum) tempSum = (long) Math.pow(--n, 2);
--- src/test/java/kata/kyu7/JadenCasingStringsTest.java
+++ src/test/java/kata/kyu7/JadenCasingStringsTest.java
@@ -10,7 +10,7 @@ public class JadenCasingStringsTest {
 
     @Test
     public void test() {
-        assertEquals("toJadenCase doesn't return a valide JadenCase String! try again please :)", jadenCase.toJadenCase("most trees are blue"), "Most Trees Are Blue");
+        assertEquals("toJadenCase doesn't return a valide JadenCase String! try again please :)", "Most Trees Are Blue", jadenCase.toJadenCase("most trees are blue"));
     }
 
     @Test
