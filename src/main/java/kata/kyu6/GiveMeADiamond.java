3276816e26478ddb4a299752366ba404aa8e67cd
Jeppe Stærk <jeppe@staerk.io>
2018-01-22 23:05:58 +0100

    Refactor

--- src/main/java/kata/kyu6/GiveMeADiamond.java
+++ src/main/java/kata/kyu6/GiveMeADiamond.java
@@ -3,7 +3,7 @@ package kata.kyu6;
 public class GiveMeADiamond {
     public static String print(int n) {
         if ((n % 2) == 0 || n <= 0) return null;
-        StringBuffer diamond = new StringBuffer();
+        StringBuilder diamond = new StringBuilder();
         for (int i = 1; i < n; i += 2) {
             for (int j = 0; j < (n - i) / 2; j++) diamond.append(" ");
             for (int k = 0; k < i; k++) diamond.append("*");
