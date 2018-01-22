f53e86770c66c7100c3bb24adeeca7e986f50bc6
Jeppe Stærk <jeppe@staerk.io>
2018-01-22 22:57:39 +0100

    Refactor

--- src/main/java/kata/kyu6/EqualSidesOfAnArray.java
+++ src/main/java/kata/kyu6/EqualSidesOfAnArray.java
@@ -2,7 +2,8 @@ package kata.kyu6;
 
 public class EqualSidesOfAnArray {
     public static int findEvenIndex(int[] arr) {
-        int index = 0, sumLeft = 0, sumRight = 0;
+        int index, sumLeft, sumRight;
+        index = 0;
         while (index++ < arr.length - 1) {
             sumLeft = sumRight = 0;
             for (int i = 0; i < index; i++) sumLeft += arr[i];
