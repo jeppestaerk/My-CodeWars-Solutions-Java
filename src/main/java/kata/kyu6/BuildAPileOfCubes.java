67add4c39eb1720813574f174622aa08f7208fa9
Jeppe Stærk <jeppe@staerk.io>
2018-01-22 22:22:32 +0100

    Refactor

--- src/main/java/kata/kyu6/BuildAPileOfCubes.java
+++ src/main/java/kata/kyu6/BuildAPileOfCubes.java
@@ -4,7 +4,7 @@ public class BuildAPileOfCubes {
     public static int findNb(long m) {
         int n = 0;
         do {
-            m = m - (long) Math.pow(n++ + 1, 3);
+            m = m - (long) Math.pow(n++ + 1d, 3d);
         } while (m > 0);
         return (m == 0) ? n : -1;
     }
