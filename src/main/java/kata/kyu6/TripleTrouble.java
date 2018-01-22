b8b0ec58f6b7411e2cd90a3f31ed80b4b7c3a49d
Jeppe Stærk <jeppe@staerk.io>
2018-01-22 22:59:15 +0100

    Refactor

--- src/main/java/kata/kyu6/TripleTrouble.java
+++ src/main/java/kata/kyu6/TripleTrouble.java
@@ -6,9 +6,8 @@ public class TripleTrouble {
         String n1 = Long.toString(num1);
         String n2 = Long.toString(num2);
         for (int i = 2; i < n1.length(); i++) {
-            if (n2.lastIndexOf(n1.charAt(i)) > 0 && (n1.charAt(i) == n1.charAt(i - 1) && n1.charAt(i) == n1.charAt(i - 2))) {
-                if (n2.charAt(n2.lastIndexOf(n1.charAt(i))) == n2.charAt(n2.lastIndexOf(n1.charAt(i)) - 1)) return 1;
-            }
+            if (n2.lastIndexOf(n1.charAt(i)) > 0 && (n1.charAt(i) == n1.charAt(i - 1) && n1.charAt(i) == n1.charAt(i - 2)) && n2.charAt(n2.lastIndexOf(n1.charAt(i))) == n2.charAt(n2.lastIndexOf(n1.charAt(i)) - 1))
+                return 1;
         }
         return 0;
     }
