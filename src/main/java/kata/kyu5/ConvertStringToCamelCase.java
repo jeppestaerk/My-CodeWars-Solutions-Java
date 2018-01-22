6e094bd64e9fba8966b1eecbceeff0ebe30ab188
Jeppe Stærk <jeppe@staerk.io>
2018-01-22 23:04:14 +0100

    Refactor

--- src/main/java/kata/kyu5/ConvertStringToCamelCase.java
+++ src/main/java/kata/kyu5/ConvertStringToCamelCase.java
@@ -3,9 +3,9 @@ package kata.kyu5;
 public class ConvertStringToCamelCase {
     static String toCamelCase(String s) {
         String[] words = s.split("[-_]");
-        String camelCase = words[0];
+        StringBuilder sb = new StringBuilder(words[0]);
         for (int i = 1; i < words.length; i++)
-            camelCase = camelCase + words[i].substring(0, 1).toUpperCase() + words[i].substring(1);
-        return camelCase;
+            sb.append(words[i].substring(0, 1).toUpperCase()).append(words[i].substring(1));
+        return sb.toString();
     }
 }
